2c55849d4b039b7a527d3ff22578449f19749bf0
masumali23020 <mdmasum0218@gmail.com>
2024-10-02 00:02:30 +0600

    sdfsdaf

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 
 
 import './App.css'
+import ClockCard from './components/shoe/ClockCard'
 import CustomColorCard from './components/shoe/CustomColorCard'
+import NikeShoe from './components/shoe/NikeShoe'
+import ProductCard from './components/shoe/ProductCard'
+import Shoe from './components/shoe/Shoe'
+import ShoeOverLay from './components/shoe/ShoeOverLay'
 
 function App() {
 
 
   return (
     <div className=' w-full h-screen'>
-    {/* <Shoe /> */}
-    {/* <ShoeOverLay /> */}
-    {/* <ProductCard /> */}
-    {/* <NikeShoe /> */}
-    {/* <ClockCard /> */}
+   <Shoe />
+    <ShoeOverLay />
+    <ProductCard />
+    <NikeShoe />
+    <ClockCard />
     <CustomColorCard />
    
     </div>
