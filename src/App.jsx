8598017b191a567aa460bff22393453406c78f5c
masumali23020 <mdmasum0218@gmail.com>
2024-10-01 14:19:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <p className="text-3xl text-slate-900 ">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
